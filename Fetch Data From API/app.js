17735b9c963f76a2913c9c0a0bf83ea274137228
Hassan-Ali0 <hassanali031935@gmail.com>
2025-05-23 15:23:01 +0500

    Fetch Data From API

--- Fetch Data From API/app.js
+++ Fetch Data From API/app.js
@@ -4,11 +4,40 @@ let data = [] ;
 
 function renderCards(){
     if(data.length < 1 ){
-        // alert('No DATA')
+        console.log('No Data')
         return
     }
 
-    console.log(data.products[0])
+    for (let i = 0; i < data.products.length; i++) {
+        let productDiv = document.createElement('div') ;
+        let productImg = document.createElement('img') ;
+        let productTitle = document.createElement('h2') ;
+        let productdDesc = document.createElement('p') ;
+        productDiv.className = 'card' ;
+        productImg.className = 'product-image' ;
+        productTitle.className = 'product-title' ;
+        productdDesc.className = 'product-Desc' ;
+
+
+
+        for(key in data.products[i]){
+            productImg.src = data.products[i].images[0] ;
+            productTitle.innerText = data.products[i].title ;
+
+            productdDesc.innerText = data.products[i].description
+        }
+
+
+
+
+
+
+
+        mainDiv.appendChild(productDiv) ;
+        productDiv.appendChild(productImg) ;
+        productDiv.appendChild(productTitle) ;
+        productDiv.appendChild(productdDesc)
+    }
 
 
 }
